ed2681f38a969d03e69b2852fd612aa0189be9b1
Alejandro Moreno <alejowoods@gmail.com>
2025-02-02 22:02:54 +0100

    Home

--- cv-frontend/src/App.jsx
+++ cv-frontend/src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';  
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';  
 import ResumeGerman from './components/ResumeGerman.jsx';
 import ResumeEnglish from './components/ResumeEnglish.jsx';
+import Home from './components/Home.jsx';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <Router>
+      <Content />
+    </Router>
+  );
+}
+
+function Content() {
+  const location = useLocation();
 
   return (
     <>
-      <Router>
         <nav>
           <ul>
+            <li>
+            <Link to="/">Home</Link>	
+            </li>
             <li>
               <Link to="/german">Lebenslauf (DE)</Link>
             </li>
@@ -21,25 +31,15 @@ function App() {
             </li>
           </ul>
         </nav>
+
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/german" element={<ResumeGerman />}></Route>
           <Route path="/english" element={<ResumeEnglish />}></Route>
         </Routes>
-      </Router>
         <div>
+          <h1>Hola lindos/as... Estoy haciendo una hoja de vida para mis nuevos trabajos. Estoy triunfando</h1>
         </div>
-        <h1>Hola lindos/as... Estoy haciendo una hoja de vida para mis nuevos trabajos. Estoy triunfando</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
     </>
   )
 }
